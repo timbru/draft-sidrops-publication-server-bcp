7b1d3d6f9b62fa08ded26d2fcaf66db03e53bbce
Tim Bruijnzeels <tim@nlnetlabs.nl>
2023-07-10 15:37:06 +0200

    Update version for publication

--- mktext.sh
+++ mktext.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DRAFT="draft-timbru-sidrops-publication-server-bcp-00"
+DRAFT="draft-timbru-sidrops-publication-server-bcp-01"
 
 mmark $DRAFT.md  > $DRAFT.xml && xml2rfc --text --html $DRAFT.xml
